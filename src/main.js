4fe8b52c91d4853ec4177b14edd091afac946ab1
jdwolf <jdwolf2@gmail.com>
2025-06-11 05:02:12 -0400

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import './style.css'
 // import './style2.css'
 
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
 import { registerLicense } from '@syncfusion/ej2-base'
 registerLicense(
   'Ngo9BigBOggjHTQxAR8/V1NMaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWX5ednVdRmNcUUJ+Wko='
